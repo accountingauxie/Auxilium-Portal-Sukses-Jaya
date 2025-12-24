9edc657b99f72b12aa15365455fc9f48370e9ad0
accountingauxie <accounting@auxie.id>
2025-12-24 12:33:08 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
-// File: assets/config.js di GitHub
-// Ganti URL di bawah ini setiap kali kamu New Deployment di App Script
+// File: assets/config.js
+// URL Script Terbaru 
 window.GAS_URL = "https://script.google.com/macros/s/AKfycbyNycnkFGHj_bHBWNz_jqfj7_C6xksFbYOgGvIioHs0e2JmqAEmJw7BkDptQuD-Y8Uy6g/exec";
